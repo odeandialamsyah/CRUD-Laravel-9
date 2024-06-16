fc217c044bf85641f5e65c9d0e297ff3d8202e58
odeandialamsyah <odeandialamsyah@gmail.com>
2024-06-16 23:53:13 +0700

    dash update

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -6,14 +6,14 @@
 <div class="flex min-h-screen ">
     {{-- Navbar  --}}
 <header>
-    <nav class="z-10 flex-none w-72 mr-20 border-b-4 h-full py-4 bg-black">
-        <div class="flex items-center justify-between">
+    <nav class=" flex-none w-64 mr-20 border-b-4 h-full py-4 bg-black">
+        <div class="flex flex-col items-start pl-7 justify-between">
             <!-- Logo -->
-            <a href="#" class="text-white hover:text-blue-600 transition duration-300 text-lg font-bold flex items-center pl-20">
-                <img src="#" alt="" class="w-16 mr-4">KOSKU
+            <a href="#" class="text-white pt-8 hover:text-blue-600 transition duration-300 text-3xl  font-bold items-center">
+            KOSKU
             </a>
             <!-- Navigation Links -->
-            <div class="space-x-5 pr-20 flex">
+            <div class="space-x-5 pt-16">
                 <!-- Jika pengguna sudah login -->
                 <div class="relative inline-block text-left">
                     <div>
@@ -21,7 +21,7 @@
                             class="group relative flex justify-center items-center text-white hover:text-blue-600 transition duration-300 font-semibold border-0 focus:outline-none"
                             id="menu-button" aria-expanded="true" aria-haspopup="true">
                             {{ Auth::user()->name }}
-                            <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
+                            <svg class="mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
                                 aria-hidden="true">
                                 <path fill-rule="evenodd"
                                     d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
@@ -51,9 +51,9 @@
     </nav>
 </header>
 {{-- Navbar end  --}}
-<div class="container mx-auto mt-10">
-    <div class="w-full h-36"></div>
-    <h1 class="text-3xl font-bold mb-6">Dashboard</h1>
+<div class="container w-auto mx-auto mt-10">
+    <div class="w-full h-5"></div>
+    <h1 class="text-3xl font-bold pb-10">DASHBOARD</h1>
 
     @if(session('success'))
         <div class="bg-green-100 border mb-6 rounded-2xl border-green-400 text-green-700 px-4 py-3 relative" role="alert">
@@ -63,16 +63,16 @@
 
     <a href="{{ route('dashboard.create') }}" class="bg-blue-600 hover:bg-blue-800 p-3 rounded-md text-white font-bold duration-200 mb-4 inline-block">Add New Item</a>
 
-    <table class="min-w-full bg-white">
+    <table class="w-full justify-center items-center bg-white">
         <thead>
             <tr>
-                <th class="py-2 px-4 border-b border-gray-200">Name</th>
-                <th class="py-2 px-4 border-b border-gray-200">Address</th>
-                <th class="py-2 px-4 border-b border-gray-200">Unit</th>
-                <th class="py-2 px-4 border-b border-gray-200">Price Range</th>
-                <th class="py-2 px-4 border-b border-gray-200">Description</th>
-                <th class="py-2 px-4 border-b border-gray-200">Photo</th>
-                <th class="py-2 px-4 border-b border-gray-200">Actions</th>
+                <th class="py-2 px-4 border border-gray-500">Name</th>
+                <th class="py-2 px-4 border border-gray-500">Address</th>
+                <th class="py-2 px-4 border border-gray-500">Unit</th>
+                <th class="py-2 px-4 border border-gray-500">Price Range</th>
+                <th class="py-2 px-4 border border-gray-500">Description</th>
+                <th class="py-2 px-4 border border-gray-500">Photo</th>
+                <th class="py-2 px-4 border border-gray-500">Actions</th>
             </tr>
         </thead>
         <tbody>
