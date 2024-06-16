187fd943d0af5f945e68ee85b6513ebae013dba9
odeandialamsyah <odeandialamsyah@gmail.com>
2024-06-17 00:04:42 +0700

    update

--- resources/views/InCard.blade.php
+++ resources/views/InCard.blade.php
@@ -7,15 +7,22 @@
     <div class="w-2/5 flex flex-col h-screen mr-10 ml-10">
         <H1 class="w-full justify-center text-center" >Gambar</H1>
     </div>
-    <div class="flex-col">
-        <H1>Deskripsi</H1>
-        <label >Name</label>
-        <label >Address</label>
-        <label >Unit</label>
-        <label >Price Range</label>
-        <label >Description</label>
-        <label >Photo</label>
-        <label >Actions</label>
+    <div class="flex flex-col">
+        <H1 class="text-2xl font-bold">Deskripsi</H1>
+        <table>
+            <label>Name</label>
+            <p></p>
+            <label>Address</label>
+            <p></p>
+            <label>Unit</label>
+            <p></p>
+            <label>Price Range</label>
+            <p></p>
+            <label>Description</label>
+            <p></p>
+            <label>Photo</label>
+            <p></p>
+        </table>
     </div>
 </div>
 
